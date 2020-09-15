f9ca4d2f48aa1848c1286af99761eed2c0d3974b
john-the-dev <52230987+john-the-dev@users.noreply.github.com>
2020-09-15 14:45:05 -0700

    Random pick
    1. Random pick problem and solution.
    2. Add assert_call_sequence method to common.py which can be used to assert call sequences for this problem. We can reuse it for other problems.

--- Array/common.py
+++ Array/common.py
@@ -3,3 +3,15 @@ def assert_list_noorder(list1, list2):
     list1.sort()
     list2.sort()
     assert(list1 == list2)
+
+# Assert a sequence of calls return expected results.
+def assert_call_sequence(context, calls, args, expected_outputs):
+    solution,output = None,[]
+    for i,callName in enumerate(calls):
+        if callName == 'Solution':
+            solution = context[callName](*args[i])
+            output.append(None)
+        else:
+            call = getattr(solution, callName)
+            output.append(call(*args[i]))
+    assert(output in expected_outputs)
